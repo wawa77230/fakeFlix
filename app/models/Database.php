0518a343ecbb7b1b86aeac3f6e9ba51d02609f24
Warren Neves <warrenneves@hotmail.fr>
2021-06-02 11:15:22 +0200

    Start

---
+++ app/models/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+
+abstract class Database
+{
+    private static $pdo;
+    private static function setBdd(){
+        self::$pdo = new PDO( "mysql:host=localhost;dbname=fakeflix; 
+             charset=UTF8",
+            "root",
+            "",
+            [
+                PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
+            ]);
+    }
+    protected function getBdd(){
+        if (self::$pdo === null){
+            self::setBdd();
+        }
+        return self::$pdo;
+    }
+}
